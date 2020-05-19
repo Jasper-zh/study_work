43e2d149c54e84c93121d9c57150e9276cb460b8
hao <1839646816@qq.com>
2020-05-19 11:16:50 +0800

    修改

--- lg1_4.java
+++ lg1_4.java
@@ -1,45 +1,44 @@
+import java.util.Arrays;
+
 /**
  * 自定义数组类
  */
 public class lg1_4 {
 
-    private static final int init_size = 8; //设定初始大小
-    private Object [] objectArray;  //存放值的数组
-    private int size;
+    private static final int init_size = 8; 
+    private Object [] objectArray = new Object[8];
+    private int size = 8;
 
-    /**
-     * 构造方法
-      */
-    public lg1_4(){
-        objectArray = new Object [init_size];
-    }
-    lg1_4(int size){
-        if (size <= 0){
-            throw new RuntimeException("数组大小不能小于等于0");
+    public static void main(String[] args){
+        lg1_4 test = new lg1_4();
+        for(int i = 0;i<30;i++){
+            Object[] a = test.add(i);
+            System.out.println(Arrays.toString(a));
+            System.out.println("实际数量:"+judgeNumber(a)+",容量大小:"+a.length);
         }
-        if (size <= 8){
-            size = init_size;
-        }
-        objectArray = new Object [size];
-    }
 
-    /**
-     * 判断实际元素个数
-     */
+	}
     public static int judgeNumber(Object[] objectArray){
-        int n = 0;  //保存元素个数的变量
+        int n = 0;
         for(int i = 0; i < objectArray.length; i++)
         {
             if(null != objectArray[i]) n++;
         }
         return n;
     }
-
-    public  void add(Object o){
-        objectArray[judgeNumber(objectArray)]=o;
+    public Object[] add(Object o){
+		int n = judgeNumber(objectArray);
+        objectArray[n]=o;
         if(judgeNumber(objectArray)>=size*0.8){
             size+=size/2;
+		    Object[] tu = objectArray;
+            objectArray = tu;
+		    objectArray = new Object[size];
+		    for(int i =0; i< tu.length; i++ ){
+		        objectArray[i] = tu[i];
+            }
         }
+		return objectArray;
     }
 
 }
--- lg1_5.java
+++ lg1_5.java
@@ -4,9 +4,18 @@ public class lg1_5 {
     }
     public static void chessboard(){
 
-       for(int i=0;i<17;i++){
+       for(int i=0;i<16;i++){
            for (int j=0;j<16;j++){
-               System.out.print("+"+"  ");
+               if(i==0&&j==0){
+                   System.out.print("   ");
+               }
+               else if(i==0){
+                   System.out.print(Integer.toHexString(j)+"  ");
+               }else if(j==0) {
+                   System.out.print(Integer.toHexString(i) + "  ");
+               }else {
+                   System.out.print("+"+"  ");
+               }
            }
            System.out.println();
        }
